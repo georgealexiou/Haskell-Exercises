9bb2b0f63ac2b5962d800601a861724e177ccac0
ga4g18 <george@alexiou.xyz>
2019-12-05 02:52:41 +0000

    Added more Tests

--- Test.hs
+++ Test.hs
@@ -37,8 +37,7 @@ simpleTests =
            (LamApp (LamAbs 4 (LamVar 4)) (LamVar 5))) 2 == 0,
         countAllReds (LamApp (LamApp (LamAbs 0 (LamAbs 1 (LamVar 0))) (LamVar 3))
            (LamApp (LamAbs 4 (LamVar 4)) (LamVar 5))) 3 == 1,
-        countAllReds (LamApp (LamApp (LamAbs 0 (LamAbs 1 (LamVar 0))) (LamVar 3))
-           (LamApp (LamAbs 4 (LamVar 4)) (LamVar 5))) 4 == 3
+        countAllReds (LamApp (LamApp (LamAbs 0 (LamAbs 1 (LamVar 0))) (LamVar 3)) (LamApp (LamAbs 4 (LamVar 4)) (LamVar 5))) 4 == 3
       ],
       [ -- Challenge 3 (but note that additional spaces will be accepted)
         printLambda (LamApp (LamVar 2) (LamVar 1)) == "x2 x1",
@@ -57,30 +56,6 @@ simpleTests =
             Just (LetDef [([1,1],LetVar 2)] (LetApp (LetFun 1) (LetVar 1))),
         parseLet "let f1 x2 = x2; f2 x1 = x1 in f1 x1" ==
             Just (LetDef [([1,2],LetVar 2),([2,1],LetVar 1)] (LetApp (LetFun 1) (LetVar 1)))
-      ],
-      [ -- Challenge 5 (but note that equivalent answers will be accepted)
-        letToLambda (LetDef [([0],LetFun 0)] (LetFun 0)) ==
-          LamApp (LamAbs 0 (LamApp (LamVar 0) (LamVar 0))) (LamAbs 0 (LamApp (LamVar 0) (LamVar 0))),
-        letToLambda (LetDef [([1,2],LetVar 2)] (LetFun 1)) ==
-          LamApp (LamAbs 0 (LamAbs 0 (LamVar 0))) (LamAbs 0 (LamAbs 0 (LamVar 0))),
-        letToLambda (LetDef [([1,2,3],LetApp (LetVar 3) (LetVar 2))] (LetFun 1)) ==
-          LamApp (LamAbs 0 (LamAbs 0 (LamAbs 1 (LamApp (LamVar 1) (LamVar 0))))) (LamAbs 0 (LamAbs 0 (LamAbs 1 (LamApp (LamVar 1) (LamVar 0))))),
-        letToLambda (LetDef [([0,0],LetFun 1),([1,1],LetVar 1)] (LetFun 0)) ==
-          LamApp (LamApp (LamAbs 0 (LamAbs 1 (LamAbs 2 (LamApp (LamApp (LamVar 1) (LamVar 0)) (LamVar 1))))) (LamAbs 0 (LamAbs 1 (LamAbs 2 (LamApp (LamApp (LamVar 1) (LamVar 0)) (LamVar 1)))))) (LamAbs 0 (LamAbs 0 (LamAbs 0 (LamVar 0)))),
-        letToLambda (LetDef [([0,0,1],LetVar 0),([1,1],LetApp (LetApp (LetFun 0) (LetVar 1)) (LetFun 1))] (LetFun 1)) ==
-          LamApp (LamApp (LamAbs 0 (LamAbs 1 (LamAbs 2 (LamApp (LamApp (LamApp (LamApp (LamVar 0) (LamVar 0)) (LamVar 1)) (LamVar 2)) (LamApp (LamApp (LamVar 1) (LamVar 0)) (LamVar 1)))))) (LamAbs 0 (LamAbs 0 (LamAbs 0 (LamAbs 1 (LamVar 0)))))) (LamAbs 0 (LamAbs 1 (LamAbs 2 (LamApp (LamApp (LamApp (LamApp (LamVar 0) (LamVar 0)) (LamVar 1)) (LamVar 2)) (LamApp (LamApp (LamVar 1) (LamVar 0)) (LamVar 1))))))  
-      ],
-      [ -- Challenge 6 (but note that equivalent answers will be accepted)
-        lambdaToLet (LamAbs 0 (LamVar 0)) ==
-          LetDef [([0,0],LetVar 0)] (LetFun 0),
-        lambdaToLet (LamApp (LamVar 1) (LamAbs 0 (LamVar 0))) ==
-          LetDef [([0,0],LetVar 0)] (LetApp (LetVar 1) (LetFun 0)),
-        lambdaToLet (LamApp (LamAbs 0 (LamVar 0)) (LamVar 1)) ==
-          LetDef [([0,0],LetVar 0)] (LetApp (LetFun 0) (LetVar 1)),
-        lambdaToLet (LamApp (LamAbs 0 (LamVar 0)) (LamAbs 0 (LamVar 0))) ==
-          LetDef [([0,0],LetVar 0),([1,0],LetVar 0)] (LetApp (LetFun 0) (LetFun 1)),
-        lambdaToLet (LamAbs 0 (LamApp (LamVar 0) (LamAbs 1 (LamApp (LamVar 0) (LamVar 1))))) ==
-          LetDef [([0,0,1],LetApp (LetVar 0) (LetVar 1)),([1,0],LetApp (LetVar 0) (LetApp (LetFun 0) (LetVar 0)))] (LetFun 1)
       ]
     ]
     
@@ -103,4 +78,6 @@ simpleTestSuite (bs : bbs) =
   do
     putStrLn ("  " ++ show (length [b | b <- bs, b]) ++ " tests passed out of " ++ show (length bs))
     simpleTestSuite bbs
+
+
 
